d882efb43dafc64dfe7f21307d74a17ea961948e
João Vitor Felix de Andrade <joao.felixandrade1201@gmail.com>
2022-06-14 16:58:05 -0300

    Estilizando navbar

--- src/components/estaticos/footer/Footer.tsx
+++ src/components/estaticos/footer/Footer.tsx
@@ -12,32 +12,20 @@ function Footer() {
         (state) => state.tokens
     );
 
-    var footerComponent;
-
-    if (token != '') {
-        footerComponent = <Grid container direction="row" justifyContent="space-between" alignItems="center" xs={12} className='box1' >
-
-            <Box className='box2' >
-                <a href="https://github.com/Projeto-SustemCity" target="_blank"><GitHubIcon className='redes' /></a>
-                <a href="sustemcity@gmail.com" target="_blank"><EmailIcon className='redes' /></a>
-            </Box>
-
-            <Box className='box3'>
-                <Typography variant="subtitle2" align="center" gutterBottom className='box3'>© 2022 Copyright:</Typography>
-                <a target="_blank" href="mailto:sustemcity@gmail.com"><Typography variant="subtitle2" gutterBottom align="center" className='box3'>Sustem City</Typography></a>
-            </Box>
-
-            <Box className='box2' >
-                <a href="https://github.com/Projeto-SustemCity" target="_blank"><GitHubIcon className='redes' /></a>
-                <a href="sustemcity@gmail.com" target="_blank"><EmailIcon className='redes' /></a>
-            </Box>
-
-        </Grid>
-    }
-
     return (
         <>
-            {footerComponent}
+            <Grid container direction="row" justifyContent="space-between" alignItems="center" xs={12} className='box1' >
+                <Box><img src="https://i.imgur.com/f509CGu.png" className='imagem' /></Box>
+                <Box className='box3'>
+                    <Typography variant="subtitle2" align="center" gutterBottom className='box3'>© 2022 Copyright:</Typography>
+                    <a target="_blank" href="mailto:sustemcity@gmail.com"><Typography variant="subtitle2" gutterBottom align="center" className='box3'>Sustem City</Typography></a>
+                </Box>
+                <Box>
+                    <a href="https://github.com/Projeto-SustemCity" target="_blank"><GitHubIcon className='redes' /></a>
+                    <a href="sustemcity@gmail.com" target="_blank"><EmailIcon className='redes' /></a>
+                </Box>
+
+            </Grid>
         </>
     )
 }
