0276d09643ff47140e6801b6f74d3d44b8fc78c9
Nay-rch <nayaraluciadarochasilva@gmail.com>
2022-06-13 21:12:15 -0300

    carrossel

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -5,6 +5,7 @@ import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { UserState } from '../../store/tokens/tokensReducer';
 import { toast } from 'react-toastify';
+import CarouselComponent from '../../components/carousel/Carousel';
 
 function Home() {
   let navigate = useNavigate();
@@ -13,24 +14,15 @@ function Home() {
   );
 
   return (
-    <>
-      <Grid container direction="row" justifyContent="center" alignItems="center" className='back'>
-        <Grid alignItems="center" item xs={6}>
-          <Box className='titulo' paddingX={20} >
-            <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" >Sejam bem vindos(as)!</Typography>
-          </Box>
-          <Box display="flex" justifyContent="center">
-            <Box marginRight={1}>
-            </Box>
-          </Box>
-        </Grid>
-        <Grid item xs={6} >
-          <Box display='flex' justifyContent='center'>
-            <img src="https://imgur.com/I37M5dQ.png" alt="Logo SustemCity" width="500vw" height="500vh" />
-          </Box>
-        </Grid>
-      </Grid>
-    </>
+
+      <>   
+  <Grid  xs={12} direction="row" justifyContent="center" alignItems="center" className='back' >
+      <Grid container item  className='grid-home' >
+      <CarouselComponent />
+      
+     </Grid>
+  </Grid>
+  </>
   );
 }
 
